05135a7a72262f72a332b281ec6cdad4be19ab0a
jawkneewin <johnnytnguyen93@gmail.com>
2023-03-08 13:34:43 -0600

    add user login, logout, and signup

--- main_app/models.py
+++ main_app/models.py
@@ -15,7 +15,7 @@ CATEGORIES = (
 class Item(models.Model):
     title = models.TextField(max_length=100)
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=1000)
     category = models.CharField(
         max_length=2,
         choices = CATEGORIES,
--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('view-cart/', views.view_cart, name='view_cart'),
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
