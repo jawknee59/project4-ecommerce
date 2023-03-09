3fdd3231c1f90ca4820fe8d1fa1481869222ebbd
jawkneewin <johnnytnguyen93@gmail.com>
2023-03-09 08:10:52 -0600

    created get total price fn for cart and render on view cart page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
     # route for items index
     path('items/', views.items_index, name='index'),
     path('items/<int:item_id>/', views.items_detail, name='detail'),
     path('view-cart/', views.view_cart, name='view_cart'),
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('accounts/signup/', views.signup, name='signup'),
 ]
